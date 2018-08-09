854254acf8c13ae78131fe9ba2302de382fdc6c3
Enrico Alvarenga <enricomalvarenga@gmail.com>
2018-08-09 00:23:26 -0300

    Closing opened file

--- filesystem/base.go
+++ filesystem/base.go
@@ -8,9 +8,10 @@ import (
 
 // Creates a virtual disk based on a path
 func CreateVirtualDisk(name string, size int32) error {
-	_, err := os.Create(name)
+	f, err := os.Create(name)
 	if err != nil {
 		return errors.Wrap(err, "Failed to create virtual disk")
 	}
+	f.Close()
 	return nil
 }
