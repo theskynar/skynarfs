506041a82f3714c0980dd7dc28b17aaae869a82d
Enrico Alvarenga <enricomalvarenga@gmail.com>
2018-09-11 08:43:47 -0300

    Refactor disk command class to abstract command actions

--- src/commands/disk/index.js
+++ src/commands/disk/index.js
@@ -1,10 +1,9 @@
 'use strict';
 
-const repl = require('vorpal-repl');
 const util = require('util');
+const colors = require('colors');
 
 const { createValidation, commonValidation } = require('./schema');
-const { fileValidation } = require('../file/schema');
 const operations = require('./operations');
 const DiskStorage = require('../../storage/disk-storage');
 
@@ -16,136 +15,123 @@ class DiskCmd {
   constructor(diskMode, replMode, storage) {
     this.diskMode = diskMode;
     this.replMode = replMode;
-    this.chalk = this.diskMode.chalk;
     this.storage = storage;
   }
 
   commands() {
-    this.create();
-    this.format();
-    this.type();
-    this.list();
-    this.enter();
+    this.diskMode.command('createdisk <name> <blocksize> <blocks>', 'DISK').action(this.create.bind(this));
+    this.diskMode.command('formatdisk <name>', 'DISK').action(this.format.bind(this));
+    this.diskMode.command('typedisk <name>', 'DISK').action(this.type.bind(this));
+    this.diskMode.command('enterdisk <name>', 'DISK').action(this.enter.bind(this));
+    this.diskMode.command('lsdisk', 'DISK').action(this.list.bind(this));
   }
 
-  create() {
-    this.diskMode
-      .command('createdisk <name> <blocksize> <blocks>', 'DISK')
-      .action(async (args, cb) => {
-        const result = createValidation(args);
-        if (result.error) {
-          const err = `\nFailed to create disk, try again:\n${util.inspect(result.error.details, false, Infinity)}\n`;
-          cb(this.chalk['red'](err));
-        }
-        try {
-          const bytes = this.storage.getAvailableBlock();
-          await operations.createDisk(args);
-          await operations.persistNewDisk(bytes, result.value);
+  async create(args, cb) {
+    const result = createValidation(args);
+    if (result.error) {
+      const err = `\nFailed to create disk, try again:\n${util.inspect(result.error.details, false, Infinity)}\n`;
+      cb(colors['red'](err));
+    }
+    try {
+      const bytes = this.storage.getAvailableBlock();
+      await operations.createDisk(args);
+      await operations.persistNewDisk(bytes, result.value);
 
-          this.storage.currentDisk = new DiskStorage(args.name);
-          this.storage.currentDisk.toBinary();
+      this.storage.currentDisk = new DiskStorage(args.name);
+      this.storage.currentDisk.toBinary();
 
-          console.info(
-            this.chalk['green'](`\n[DISK] Virtual disk ${args.name} with size ${args.blocks * args.blocksize} created successfully\n`)
-          );
-          cb(null);
-        } catch (err) {
-          cb(this.chalk['red'](err));
-        }
-      });
+      console.info(
+        colors['green'](`\n[DISK] Virtual disk ${args.name} with size ${args.blocks * args.blocksize} created successfully\n`)
+      );
+
+      cb(null);
+    } catch (err) {
+      cb(colors['red'](err));
+    }
   }
 
-  format() {
-    this.diskMode
-      .command('formatdisk <name>', 'DISK')
-      .action(async (args, cb) => {
-        const result = commonValidation(args);
-        if (result.error) {
-          const err = `\nFailed to format disk, try again:\n${util.inspect(result.error.details, false, Infinity)}\n`;
-          cb(this.diskMode.chalk['red'](err));
-        }
+  async format(args, cb) {
+    const result = commonValidation(args);
+    if (result.error) {
+      const err = `\nFailed to format disk, try again:\n${util.inspect(result.error.details, false, Infinity)}\n`;
+      cb(colors['red'](err));
+    }
 
-        try {
-          const disk = this.storage.mainDisksInfo[result.value.name];
-          if (!disk) {
-            cb(this.chalk['yellow'](`\nThe disk ${result.value.name} does not exist\n`));
-          }
+    try {
+      const disk = this.storage.mainDisksInfo[result.value.name];
+      if (!disk) {
+        cb(colors['yellow'](`\nThe disk ${result.value.name} does not exist\n`));
+      }
 
-          await operations.formatDisk(disk);
-          console.info(this.chalk['green'](`\n[DISK] Virtual disk ${args.name} was formatted\n`));
-          cb();
-        } catch (err) {
-          cb(this.chalk['red'](`\nFailed to format disk ${err}\n`));
-        }
-      });
+      await operations.formatDisk(disk);
+      console.info(colors['green'](`\n[DISK] Virtual disk ${args.name} was formatted\n`));
+      cb();
+    } catch (err) {
+      cb(colors['red'](`\nFailed to format disk ${err}\n`));
+    }
   }
 
-  type() {
-    this.diskMode
-      .command('typedisk <name>', 'DISK')
-      .action(async (args, cb) => {
-        const result = commonValidation(args);
-        if (result.error) {
-          const err = `\nFailed to fetch content from the disk, 
+  async type(args, cb) {
+    const result = commonValidation(args);
+    if (result.error) {
+      const err = `\nFailed to fetch content from the disk, 
             try again:\n${util.inspect(result.error.details, false, Infinity)}\n`;
-          cb(this.diskMode.chalk['red'](err));
-        }
+      cb(colors['red'](err));
+    }
 
-        try {
-          const disk = this.storage.mainDisksInfo[result.value.name];
-          if (!disk) {
-            cb(this.chalk['yellow'](`\nThe disk ${result.value.name} does not exist\n`));
-          }
+    try {
+      const disk = this.storage.mainDisksInfo[result.value.name];
+      if (!disk) {
+        cb(colors['yellow'](`\nThe disk ${result.value.name} does not exist\n`));
+      }
 
-          const hasContent = await operations.typeDisk(disk, this.chalk['green']);
-          if (!hasContent) {
-            console.info(this.chalk['green'](`\n[DISK] Virtual disk ${args.name} HAS NO CONTENT\n`));
-          }
-          cb();
-        } catch (err) {
-          cb(this.chalk['red'](`\nFailed to format disk ${err}\n`));
-        }
-        cb();
-      });
+      const hasContent = await operations.typeDisk(disk, colors['green']);
+      if (!hasContent) {
+        console.info(colors['green'](`\n[DISK] Virtual disk ${args.name} HAS NO CONTENT\n`));
+      }
+      cb();
+    } catch (err) {
+      cb(colors['red'](`\nFailed to format disk ${err}\n`));
+    }
   }
 
-  list() {
-    this.diskMode
-      .command('lsdisk', 'DISK')
-      .action((args, cb) => {
-        console.log(this.chalk['green']('\nAvailable disks: '));
-        const disks = Object.keys(this.storage.mainDisksInfo);
-        const data = this.storage.mainDisksInfo;
-        for (let i = 0; i < disks.length; i++) {
-          console.log(this.chalk['blue'](`${disks[i]} - ${data[disks[i]].blocks * data[disks[i]].blocksize} bytes`));
-        }
-        console.log();
-        cb();
-      });
+  list(args, cb) {
+    try {
+      console.log(colors['green']('\nAvailable disks: '));
+      const disks = Object.keys(this.storage.mainDisksInfo);
+      const data = this.storage.mainDisksInfo;
+      for (let i = 0; i < disks.length; i++) {
+        console.log(colors['blue'](`${disks[i]} - ${data[disks[i]].blocks * data[disks[i]].blocksize} bytes`));
+      }
+      console.log();
+      cb();
+    } catch (err) {
+      cb(new Error(colors.red(`Listing errors: ${err}`)));
+    }
   }
 
-  enter() {
-    this.diskMode
-      .command('enterdisk <name>', 'DISK')
-      .delimiter()
-      .action((args, cb) => {
-        const result = commonValidation(args);
-        if (result.error) {
-          const err = `\nFailed to enter disk, 
-            try again:\n${util.inspect(result.error.details, false, Infinity)}\n`;
-          cb(this.diskMode.chalk['red'](err));
-        }
+  async enter(args, cb) {
+    try {
+      const result = commonValidation(args);
+      if (result.error) {
+        const err = `\nFailed to enter disk, 
+              try again:\n${util.inspect(result.error.details, false, Infinity)}\n`;
+        cb(colors['red'](err));
+      }
 
-        if (!this.storage.mainDisksInfo[args.name]) {
-          cb(this.diskMode.chalk['red']('Disk was not found'));
-          return;
-        }
+      const disk = this.storage.mainDisksInfo[args.name];
+      if (!disk) {
+        cb(colors['red']('Disk was not found'));
+        return;
+      }
 
-        this.storage.currentDisk = new DiskStorage(args.name);
-        this.storage.currentDisk.fromBinary();
+      this.storage.currentDisk = new DiskStorage(args.name, disk.blocks, disk.blocksize);
+      this.storage.currentDisk.fromBinary();
 
-        this.replMode.delimiter(`$skynarfs:${args.name}> `).show();
-      });
+      this.replMode.delimiter(`$skynarfs:${args.name}> `).show();
+    } catch (err) {
+      cb(new Error(colors.red(`Entering disk errors: ${err}`)));
+    }
   }
 }
 
